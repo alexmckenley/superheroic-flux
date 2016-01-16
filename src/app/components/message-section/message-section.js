cb25d030eb4a03641fefde6aff6383bff1698f58
Alex Taylor <alexmckenley@gmail.com>
2016-01-16 15:35:03 -0800

    Update component templates

--- src/app/components/message-section/message-section.js
+++ src/app/components/message-section/message-section.js
@@ -2,7 +2,7 @@ angular.module( 'shf.components.message-section', [])
 
 .directive( 'messageSection', function messageSectionDirective() {
   return {
-    template: '<h2>{{ \'hello\' }} Message Section</h2>'
+    templateUrl: 'components/message-section/message-section.tpl.html'
   };
 });
 
--- src/app/components/thread-section/thread-section.js
+++ src/app/components/thread-section/thread-section.js
@@ -2,7 +2,7 @@ angular.module( 'shf.components.thread-section', [])
 
 .directive( 'threadSection', function threadSectionDirective() {
   return {
-    template: '<h2>Thread Section</h2>'
+    templateUrl: 'components/thread-section/thread-section.tpl.html'
   };
 });
 
